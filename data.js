9762adc7810bc5359747fb62dbc06877153e3947
Ana Burcovschi <ana@burcovschi.com>
2024-11-20 19:28:40 +0200

    innit

---
+++ data.js
@@ -0,0 +1,23 @@
+let songs = [
+    {
+        name: "Orion",
+        path: "./audio/Orion.mp3",
+        artist: "Metalica",
+        album: "./img/Master Of Puppets.jpeg",
+        background: "./img/metalica.png"
+    },
+    {
+        name: "Master Of Puppets",
+        path: "./audio/Master Of Puppets.mp3",
+        artist: "Metalica",
+        album: "./img/Master Of Puppets.jpeg",
+        background: "./img/metalica.png"
+    },
+    {
+        name: "Back In Black",
+        path: "./audio/Back In Black.mp3",
+        artist: "AC/DC",
+        album: "./img/Back in Black.jpeg",
+        background: "./img/ACDC.webp"
+    }
+]
--- script.js
+++ script.js
@@ -1,29 +1,76 @@
 const progress = document.querySelector("#song-progress");
-const song = document.querySelector("#song");
-const playBtn = document.querySelector("#play");
-const skipPrev = document.querySelector("#skip-prev");
-const skipNext = document.querySelector("#skip-next");
+const player = document.querySelector("#player-img")
 
-song.addEventListener("loadedmetadata", playSong);
-song.addEventListener("durationchange", () => {
+const music = document.querySelector("#song");
+const songName = document.querySelector("#song-name");
+const songArtist = document.querySelector("#song-artist");
+const songImg = document.querySelector("#song-img")
+const songCurrentTime = document.querySelector("#current-time");
+const songDuration = document.querySelector("#song-duration");
+
+const volume = document.querySelector("#volume");
+const volumeImg = document.querySelector("#volume-img");
+
+const playBtn = document.querySelector("#play");
+const skipPrevBtn = document.querySelector("#skip-prev");
+const skipNextBtn = document.querySelector("#skip-next");
+
+let musicIndex = 0;
+
+function setSong(i){
+    progress.value = 0;
+    let song = songs[i];
+    musicIndex = i;
+    music.src = song.path;
+    songName.innerHTML = song.name;
+    songArtist.innerHTML = song.artist;
+    songImg.src = song.album;
+    songCurrentTime.innerHTML = "00:00";
+    player.src = song.background;
+    setInterval(() => {
+        songDuration.innerHTML = formatTime(music.duration);
+    }, 300)
+    // songDuration.innerHTML = formatTime(music.duration);
+}
+setSong(0)
+
+function formatTime(time){
+    let min = Math.floor(time / 60);
+    if(min < 10){
+        min = `0${min}`;
+    } 
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0${sec}`;
+    }
+    return `${min} : ${sec}`;
+}
+
+music.addEventListener("loadedmetadata", playSong);
+music.addEventListener("durationchange", () => {
     playSong()
 });
 
 function playSong() {
-    progress.max = song.duration;
-    progress.value = song.currentTime;
-}
-setInterval(() => {
-     if (!song.paused) {
+    progress.max = music.duration;
+    progress.value = music.currentTime;
+    setInterval(() => {
+     if (!music.paused) {
         playSong()
     }
-}, 500);
+    songCurrentTime.innerHTML = formatTime(music.currentTime)
+    if(Math.floor(music.currentTime) == Math.floor(progress.max)){
+        skipNextSong();
+    }
+}, 1000);
+}
+
 
 progress.addEventListener("input", progressSong); 
 function progressSong() {
-    song.play();
-    song.currentTime = progress.value;
-    playBtn.src = "./svg/pause.svg"; 
+    music.play();
+    music.currentTime = progress.value;
+    playBtn.classList.remove("paused"); 
 }
 
 
@@ -31,11 +78,57 @@ playBtn.addEventListener("click", playPause);
 
 function playPause() {
 
-    if (this.src.includes("/svg/play.svg")) {
-        song.play();
-        this.src = "./svg/pause.svg"; 
+    if (this.classList.contains("paused")) {
+        playMusic()
     } else {
-        song.pause();
-        this.src = "./svg/play.svg"; 
+        music.pause();
+        this.classList.add("paused");; 
     }
+}
+
+skipNextBtn.addEventListener("click", skipNextSong);
+skipPrevBtn.addEventListener("click", skipPrevSong);
+ 
+function skipNextSong(){
+    if(musicIndex >= songs.length - 1){
+        musicIndex = 0;
+    } else{
+        musicIndex++
+    }
+    setSong(musicIndex);
+    playMusic();
+}
+
+function skipPrevSong() {
+    if(musicIndex <= 0){
+        musicIndex = songs.length - 1;
+    } else{
+        musicIndex--
+    }
+    setSong(musicIndex);
+    playMusic();
+}
+function playMusic(){
+    music.play();
+    playBtn.classList.remove("paused"); 
+}
+volumeImg.addEventListener("click", muteVolume);
+volume.addEventListener("input", songVolume);
+
+function songVolume(){
+    music.volume = volume.value / 100;
+    if(volume.value >= 75){
+        volumeImg.src = "./svg/volume-3.svg";
+    } else if(volume.value < 75 && volume.value >= 50){
+        volumeImg.src = "./svg/volume-2.svg";
+    } else if(volume.value < 50 && volume.value >= 20){
+        volumeImg.src = "./svg/volume-1.svg";
+    } else if(volume.value < 20){
+        volumeImg.src = "./svg/volume-0.svg";
+    }
+}
+function muteVolume(){
+    music.volume = 0;
+    volume.value = 0;
+    volumeImg.src = "./svg/volume-0.svg";
 }
