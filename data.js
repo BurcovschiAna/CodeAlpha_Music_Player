c34d68c1133682b52a656cdb74dba1f860ede6a1
Ana Burcovschi <ana@burcovschi.com>
2024-11-30 17:46:50 +0200

    adding coments

--- data.js
+++ data.js
@@ -4,98 +4,112 @@ let songs = [
         path: "./audio/Orion.mp3",
         artist: "Metallica",
         album: "./img/Master Of Puppets.jpeg",
-        background: "./img/Metallica orion.jpeg"
+        background: "./img/Metallica orion.jpeg",
+        dataIndex: 0
     },
     {
         name: "Master Of Puppets",
         path: "./audio/Master Of Puppets.mp3",
         artist: "Metallica",
         album: "./img/Master Of Puppets.jpeg",
-        background: "./img/Metallica orion.jpeg"
+        background: "./img/Metallica orion.jpeg",
+        dataIndex: 1
     },
     {
         name: "Back In Black",
         path: "./audio/Back In Black.mp3",
         artist: "AC/DC",
         album: "./img/Back in Black.jpeg",
-        background: "./img/ACDC back in black.jpg"
+        background: "./img/ACDC back in black.jpg",
+        dataIndex: 2
     },
     {
         name: "The House of The Risung Sun",
         path: "./audio/Animals - The House Of The Rising Sun.mp3",
         artist: "Animals",
         album: "./img/House of the rising sun.jpg",
-        background: "./img/Animals.jpeg"
+        background: "./img/Animals.jpeg",
+        dataIndex: 3
     }, 
     {
         name: "Bad Medicine",
         path: "./audio/Bon Jovi - Bad Medicine.mp3",
         artist: "Bon Jovi",
         album: "./img/Bad medicine.jpg",
-        background: "./img/Bon Jovi bad medicine.webp"
+        background: "./img/Bon Jovi bad medicine.webp",
+        dataIndex: 4
     },
     {
         name: "It's My Life",
         path: "./audio/Bon Jovi - It's My Life.mp3",
         artist: "Bon Jovi",
         album: "./img/Its my life.jpg",
-        background: "./img/bon jovi its my life.jpg"
+        background: "./img/bon jovi its my life.jpg",
+        dataIndex: 5
     },
     {
         name: "Livin' On A Prayer",
         path: "./audio/Bon Jovi - Livin' On A Prayer.mp3",
         artist: "Bon Jovi",
         album: "./img/Livin' On A Prayer.jpg",
-        background: "./img/Bon Jovi Livin' On A Prayer.webp"
+        background: "./img/Bon Jovi Livin' On A Prayer.webp",
+        dataIndex: 6
     },
     {
-        name: " Wanted Dead Or Alive",
+        name: "Wanted Dead Or Alive",
         path: "./audio/Bon Jovi - Wanted Dead Or Alive.mp3",
         artist: "Bon Jovi",
         album: "./img/Livin' On A Prayer.jpg",
-        background: "./img/Bon Jovi Livin' On A Prayer.webp"
+        background: "./img/Bon Jovi Livin' On A Prayer.webp",
+        dataIndex: 7
     },
     {
-        name: " You Give Love A Bad Name",
+        name: "You Give Love A Bad Name",
         path: "./audio/Bon Jovi - You Give Love A Bad Name.mp3",
         artist: "Bon Jovi",
         album: "./img/Livin' On A Prayer.jpg",
-        background: "./img/Bon Jovi Livin' On A Prayer.webp"
+        background: "./img/Bon Jovi Livin' On A Prayer.webp",
+        dataIndex: 8
     },
     {
         name: "Come as you are",
         path: "./audio/Come As You Are - Nirvana.mp3",
         artist: "Nirvana",
         album: "./img/come as you are.jpg",
-        background: "./img/Nirvana come as you are.webp"
+        background: "./img/Nirvana come as you are.webp",
+        dataIndex: 9
     },
     {
         name: "Enter Sandman",
         path: "./audio/Enter Sandman - Metalica.mp3",
         artist: "Metallica",
         album: "./img/Enter Sadman.jpg",
-        background: "./img/Metalica enter sadman.webp"
+        background: "./img/Metalica enter sadman.webp",
+        dataIndex: 10
     },
     {
         name: "For Whom The Bell Tolls",
         path: "./audio/For Whom The Bell Tolls - Metalica.mp3",
         artist: "Metallica",
         album: "./img/For whom the bell tolls.jpg",
-        background: "./img/Metallica For Whom The Bell Tolls.jpg"
+        background: "./img/Metallica For Whom The Bell Tolls.jpg",
+        dataIndex: 11
     },
     {
         name: "Heaven's On Fire",
         path: "./audio/Heaven's On Fire - Kiss.mp3",
         artist: "Kiss",
         album: "./img/Heaven's On Fire.jpg",
-        background: "./img/Kiss Heaven's On Fire.jpg"
+        background: "./img/Kiss Heaven's On Fire.jpg",
+        dataIndex: 12
     },
     {
         name: "Highway to Hell",
         path: "./audio/Highway to Hell - ACDC.mp3",
         artist: "AC/DC",
         album: "./img/Highway to Hell.jpg",
-        background: "./img/ACDC.webp"
+        background: "./img/ACDC.webp",
+        dataIndex: 13
     },
 
     {
@@ -103,132 +117,151 @@ let songs = [
         path: "./audio/I Was Made For Lovin' You - Kiss.mp3",
         artist: "Kiss",
         album: "./img/I Was Made For Lovin' You.jpg",
-        background: "./img/Kiss I Was Made For Lovin' You.jpg"
+        background: "./img/Kiss I Was Made For Lovin' You.jpg",
+        dataIndex: 14
     },
     {
         name: "Knockin' On Heaven's Door",
         path: "./audio/Knockin' On Heaven's Door - Guns n Roses.mp3",
         artist: "Guns N Roses",
         album: "./img/Knockin' On Heaven's Door.jpg",
-        background: "./img/Guns n Roses Knockin' On Heaven's Door.jpg"
+        background: "./img/Guns n Roses Knockin' On Heaven's Door.jpg",
+        dataIndex: 15
     },
     {
         name: "Another one bites the dust",
         path: "./audio/Queen - Another one bites the dust.mp3",
         artist: "Queen",
         album: "./img/Another_one_bites_the_dust.jpg",
-        background: "./img/Queen Another one bites the dust.webp"
+        background: "./img/Queen Another one bites the dust.webp",
+        dataIndex: 16
     },
     {
         name: "Bohemian Rhapsody",
         path: "./audio/Queen - Bohemian Rhapsody.mp3",
         artist: "Queen",
         album: "./img/Bohemian Rhapsody.jpg",
-        background: "./img/Queen Bohemian Rhapsody.jpg"
+        background: "./img/Queen Bohemian Rhapsody.jpg",
+        dataIndex: 17
     },
     {
         name: "Don't Stop Me Now",
         path: "./audio/Queen - Don't Stop Me Now.mp3",
         artist: "Queen",
         album: "./img/Don't Stop Me Now.jpg",
-        background: "./img/Queen Don't Stop Me Now.webp"
+        background: "./img/Queen Don't Stop Me Now.webp",
+        dataIndex: 18
     },
     {
         name: "I Want It All",
         path: "./audio/Queen - I Want It All.mp3",
         artist: "Queen",
         album: "./img/I Want It All.jpg",
-        background: "./img/Queen I Want It All.webp"
+        background: "./img/Queen I Want It All.webp",
+        dataIndex: 19
     },
     {
         name: "I Want To Break Free",
         path: "./audio/Queen - I Want To Break Free.mp3",
         artist: "Queen",
         album: "./img/I Want To Break Free.jpg",
-        background: "./img/Queen I Want To Break Free.png"
+        background: "./img/Queen I Want To Break Free.png",
+        dataIndex: 20
     },
     {
         name: "Killer Queen",
         path: "./audio/Queen - Killer Queen.mp3",
         artist: "Queen",
         album: "./img/Killer Queen.webp",
-        background: "./img/Queen Killer Queen.webp"
+        background: "./img/Queen Killer Queen.webp",
+        dataIndex: 21
     },
     {
         name: "Radio Ga Ga",
         path: "./audio/Queen - Radio Ga Ga.mp3",
         artist: "Queen",
         album: "./img/Radio Ga Ga.jpg",
-        background: "./img/Queen I Want To Break Free.png"
+        background: "./img/Queen I Want To Break Free.png",
+        dataIndex: 22
     },
     {
         name: "The Show Must Go On",
         path: "./audio/Queen - The Show Must Go On.mp3",
         artist: "Queen",
         album: "./img/The Show Must Go On.png",
-        background: "./img/Queen The Show Must Go On.jpg"
+        background: "./img/Queen The Show Must Go On.jpg",
+        dataIndex: 23
     },
     {
         name: "Under Pressure",
         path: "./audio/Queen - Under Pressure.mp3",
         artist: "Queen",
         album: "./img/Under Pressure.jpg",
-        background: "./img/Queen Under Pressure.webp"
+        background: "./img/Queen Under Pressure.webp",
+        dataIndex: 24
     },
     {
         name: "Rock And Roll All Nite",
         path: "./audio/Rock And Roll All Nite - Kiss.mp3",
         artist: "Kiss",
         album: "./img/Rock And Roll All Nite.jpg",
-        background: "./img/Kiss Rock And Roll All Nite.jpg"
+        background: "./img/Kiss Rock And Roll All Nite.jpg",
+        dataIndex: 25
     },
     {
         name: "Rock N Roll Train",
         path: "./audio/Rock N Roll Train - ACDC.mp3",
         artist: "AC/DC",
         album: "./img/Rock N Roll Train.jpg",
-        background: "./img/AC DC.jpg"
+        background: "./img/AC DC.jpg",
+        dataIndex: 26
     },
     {
         name: "Smells Like Teen Spirit",
         path: "./audio/Smells Like Teen Spirit -Nirvana.mp3",
         artist: "Nirvana",
         album: "./img/Smells Like Teen Spirit.jpg",
-        background: "./img/Nirvana Smells Like Teen Spirit.jpg"
+        background: "./img/Nirvana Smells Like Teen Spirit.jpg",
+        dataIndex: 27
     },
     {
         name: "Sweet Child O' Mine",
         path: "./audio/Sweet Child O' Mine - Guns n Roses.mp3",
         artist: "Guns N Roses",
         album: "./img/Sweet Child O' Mine.png",
-        background: "./img/Guns n Roses Sweet Child O' Mine.webp"
+        background: "./img/Guns n Roses Sweet Child O' Mine.webp",
+        dataIndex: 28
     },
     {
         name: "The Unforgiven",
         path: "./audio/The Unforgiven - Metalica.mp3",
         artist: "Metallica",
         album: "./img/Enter Sadman.jpg",
-        background: "./img/Metalica enter sadman.webp"
+        background: "./img/Metalica enter sadman.webp",
+        dataIndex: 29
     },
     {
         name: "Thunderstruck",
         path: "./audio/Thunderstruck - ACDC.mp3",
         artist: "AC/DC",
         album: "./img/Thunderstruck.jpg",
-        background: "./img/ACDC Thunderstruck.jpg"
+        background: "./img/ACDC Thunderstruck.jpg",
+        dataIndex: 30
     },
     {
         name: "TNT",
         path: "./audio/TNT - ACDC.mp3",
         artist: "AC/DC",
         album: "./img/TNT.jpg",
-        background: "./img/ACDC TNT.jpg"
+        background: "./img/ACDC TNT.jpg",
+        dataIndex: 31
     },
     {
         name: "Welcome To The Jungle",
         path: "./audio/Welcome To The Jungle - Guns n Roses.mp3",
         artist: "Guns N Roses",
         album: "./img/Sweet Child O' Mine.png",
-        background: "./img/Guns n Roses Welcome To The Jungle.webp"
+        background: "./img/Guns n Roses Welcome To The Jungle.webp",
+        dataIndex: 32
     }
 ];
--- script.js
+++ script.js
@@ -1,160 +1,234 @@
+// Select DOM elements for controlling the music player
 const progress = document.querySelector("#song-progress");
-const player = document.querySelector("#player-img")
-
+const player = document.querySelector("#player-img");
 const music = document.querySelector("#song");
 const songName = document.querySelector("#song-name");
 const songArtist = document.querySelector("#song-artist");
-const songImg = document.querySelector("#song-img")
+const songImg = document.querySelector("#song-img");
 const songCurrentTime = document.querySelector("#current-time");
 const songDuration = document.querySelector("#song-duration");
-
 const volume = document.querySelector("#volume");
 const volumeImg = document.querySelector("#volume-img");
-
 const playBtn = document.querySelector("#play-btn");
 const skipPrevBtn = document.querySelector("#skip-prev");
 const skipNextBtn = document.querySelector("#skip-next");
 
+// Initialize the music index and playlist container
 let musicIndex = 0;
+const playlistContainer = document.querySelector("#playlist-container");
 
-function setSong(i){
-    progress.value = 0;
-    let song = songs[i];
-    musicIndex = i;
-    music.src = song.path;
-    songName.innerHTML = song.name;
-    songArtist.innerHTML = song.artist;
-    songImg.src = song.album;
-    songCurrentTime.innerHTML = "00:00";
-    player.src = song.background;
-    setInterval(() => {
-        songDuration.innerHTML = formatTime(music.duration);
-    }, 300)
+// Display all songs in the playlist
+displayAllSongs(songs);
+
+// Set up search functionality
+const search = document.querySelector("#search");
+let mySet = new Set();
+for (let i = 0; i < songs.length; i++) {
+    mySet.add(songs[i].name);
+    mySet.add(songs[i].artist);
 }
-setSong(0)
 
-function formatTime(time){
+// Convert the Set to an Array for easier manipulation
+let allSongs = Array.from(mySet);
+
+// Add event listener for the search input
+search.addEventListener("keyup", autocomplete);
+
+// Set up event listeners for music metadata and controls
+music.addEventListener("loadedmetadata", playSong);
+music.addEventListener("durationchange", () => {
+    playSong();
+});
+progress.addEventListener("input", progressSong);
+playBtn.addEventListener("click", playPause);
+skipNextBtn.addEventListener("click", skipNextSong);
+skipPrevBtn.addEventListener("click", skipPrevSong);
+volumeImg.addEventListener("click", muteVolume);
+volume.addEventListener("input", songVolume);
+
+// Function to display all songs in the playlist
+function displayAllSongs(songsToDisplay) {
+    playlistContainer.innerHTML = ""; // Clear the playlist container
+    for (let i = 0; i < songsToDisplay.length; i++) {
+        let a = i + 1;
+        if (a < 10) {
+            a = "0" + a; // Format index to two digits
+        }
+        let songElement = document.createElement("div"); // Create a new song element
+        songElement.classList.add("song", "flexbox");
+        songElement.setAttribute("data-index", songsToDisplay[i].dataIndex);
+        playlistContainer.appendChild(songElement); // Append song element to the container
+        songElement.innerHTML += `<span>${a}</span>
+            <img src="${songsToDisplay[i].album}" class="playlist-album-cover">
+            <div class="playlist-song-details">
+                <p>${songsToDisplay[i].name}</p> 
+                <p>${songsToDisplay[i].artist}</p>
+            </div>
+            <div class="flexbox">
+                <div class="playin-song hidden flexbox">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <img src="./svg/play.svg" alt="" class="play-song">
+            </div>`;
+    }
+    // Add click event to each song element
+    const songElements = document.querySelectorAll(".song");
+    songElements.forEach(song => {
+        song.addEventListener("click", selectSong);
+    });
+}
+
+// Function to set the current song based on index
+function setSong(i) {
+    progress.value = 0; // Reset progress
+    let song = songs[i]; // Get the song object
+    musicIndex = i; // Update the current music index
+    music.src = song.path; // Set the music source
+    songName.innerHTML = song.name; // Display song name
+    songArtist.innerHTML = song.artist; // Display artist name
+    songImg.src = song.album; // Set album cover
+    songCurrentTime.innerHTML = "00:00"; // Reset current time display
+    player.src = song.background; // Set background image
+    setInterval(() => {
+        songDuration.innerHTML = formatTime(music.duration); // Update song duration
+    }, 300);
+    playState(document.querySelectorAll(".song")[musicIndex]); // Update play state UI
+}
+
+// Function to format time in mm:ss
+function formatTime(time) {
     let min = Math.floor(time / 60);
-    if(min < 10){
+    if (min < 10) {
         min = `0${min}`;
-    } 
+    }
     let sec = Math.floor(time % 60);
-    if(sec < 10){
+    if (sec < 10) {
         sec = `0${sec}`;
     }
     return `${min} : ${sec}`;
 }
 
-music.addEventListener("loadedmetadata", playSong);
-music.addEventListener("durationchange", () => {
-    playSong()
-});
-
+// Function to play the song and update the progress
 function playSong() {
-    progress.max = music.duration;
-    progress.value = music.currentTime;
+    progress.max = music.duration; // Set the maximum value of progress
+    progress.value = music.currentTime; // Update progress value
     setInterval(() => {
-     if (!music.paused) {
-        playSong()
-    }
-    songCurrentTime.innerHTML = formatTime(music.currentTime)
-    if(Math.floor(music.currentTime) == Math.floor(progress.max)){
-        skipNextSong();
-    }
-}, 1000);
+        if (!music.paused) {
+            playSong(); // Recursively call playSong if music is playing
+        }
+        songCurrentTime.innerHTML = formatTime(music.currentTime); // Update current time
+        if (Math.floor(music.currentTime) == Math.floor(progress.max)) {
+            skipNextSong(); // Skip to the next song when current time reaches max
+        }
+    }, 1000);
 }
 
-
-progress.addEventListener("input", progressSong); 
+// Function to update song progress based on user input
 function progressSong() {
-    music.play();
-    music.currentTime = progress.value;
-    playBtn.classList.remove("paused"); 
+    music.play(); // Play music
+    music.currentTime = progress.value; // Set current time based on progress input
+    playBtn.classList.remove("paused"); // Update play button state
 }
 
-
-playBtn.addEventListener("click", playPause);
-
+// Function to toggle play/pause state
 function playPause() {
-
     if (this.classList.contains("paused")) {
-        playMusic()
+        playMusic(); // Play music if paused
     } else {
-        music.pause();
-        this.classList.add("paused");; 
+        music.pause(); // Pause music if playing
+        this.classList.add("paused"); // Update play button state
     }
 }
 
-skipNextBtn.addEventListener("click", skipNextSong);
-skipPrevBtn.addEventListener("click", skipPrevSong);
- 
-function skipNextSong(){
-    if(musicIndex >= songs.length - 1){
-        musicIndex = 0;
-    } else{
-        musicIndex++
+// Function to skip to the next song
+function skipNextSong() {
+    if (musicIndex >= songs.length - 1) {
+        musicIndex = 0; // Loop back to the first song
+    } else {
+        musicIndex++; // Move to the next song
     }
-    setSong(musicIndex);
-    playMusic();
+    setSong(musicIndex); // Set the new song
+    playMusic(); // Play the new song
 }
 
+// Function to skip to the previous song
 function skipPrevSong() {
-    if(musicIndex <= 0){
-        musicIndex = songs.length - 1;
-    } else{
-        musicIndex--
+    if (musicIndex <= 0) {
+        musicIndex = songs.length - 1; // Loop to the last song
+    } else {
+        musicIndex--; // Move to the previous song
     }
-    setSong(musicIndex);
-    playMusic();
+    setSong(musicIndex); // Set the new song
+    playMusic(); // Play the new song
 }
-function playMusic(){
-    music.play();
-    playBtn.classList.remove("paused"); 
-}
-volumeImg.addEventListener("click", muteVolume);
-volume.addEventListener("input", songVolume);
 
-function songVolume(){
-    music.volume = volume.value / 100;
-    if(volume.value >= 75){
+// Function to play music
+function playMusic() {
+    music.play(); // Play the music
+    playBtn.classList.remove("paused"); // Update play button state
+}
+
+// Function to adjust the volume
+function songVolume() {
+    music.volume = volume.value / 100; // Set music volume
+    // Update volume icon based on volume level
+    if (volume.value >= 75) {
         volumeImg.src = "./svg/volume-3.svg";
-    } else if(volume.value < 75 && volume.value >= 50){
+    } else if (volume.value < 75 && volume.value >= 50) {
         volumeImg.src = "./svg/volume-2.svg";
-    } else if(volume.value < 50 && volume.value >= 20){
+    } else if (volume.value < 50 && volume.value >= 20) {
         volumeImg.src = "./svg/volume-1.svg";
-    } else if(volume.value < 20){
+    } else if (volume.value < 20) {
         volumeImg.src = "./svg/volume-0.svg";
     }
 }
-function muteVolume(){
-    music.volume = 0;
-    volume.value = 0;
-    volumeImg.src = "./svg/volume-0.svg";
+
+// Function to mute the volume
+function muteVolume() {
+    music.volume = 0; // Mute music
+    volume.value = 0; // Reset volume slider
+    volumeImg.src = "./svg/volume-0.svg"; // Update volume icon
 }
 
-const playlistContainer = document.querySelector("#playlist-container");
-
-
-
-function displayAllSongs() {
-    for(let i = 0; i < songs.length; i++){
-        let a = i + 1;
-        if(a < 10){
-            a = "0" + a;
-        }
-        playlistContainer.innerHTML += `
-        <div class="song flexbox">
-            <span>${a}</span>
-            <img src="${songs[i].album}" class="playlist-album-cover">
-            <div class="playlist-song-details">
-                <p>${songs[i].name}</p> 
-                <p>${songs[i].artist}</p>
-            </div>
-            <div id="play-song" class="flexbox">
-                <img src="./svg/play.svg" alt="">
-            </div>
-        </div>
-         `
+// Function for autocomplete search feature
+function autocomplete() {
+    let value = search.value.trim().toLowerCase(); // Get the search input
+    if (value) {
+        // Filter songs based on search input
+        let filteredSongs = songs.filter(function(song) {
+            return song.name.toLowerCase().indexOf(value) === 0 || 
+                   song.artist.toLowerCase().indexOf(value) === 0;
+        });
+        displayAllSongs(filteredSongs); // Display filtered songs
+    } else {
+        displayAllSongs(songs); // Display all songs if no input
     }
-  }
-  displayAllSongs()
+}
+
+// Function to select a song from the playlist
+function selectSong() { 
+    let index = this.getAttribute("data-index"); // Get the index of the selected song
+    setSong(index); // Set the selected song
+    playMusic(); // Play the selected song
+}
+
+// Function to update the play state UI
+function playState(parent) {
+    // Reset play button visibility for all songs
+    document.querySelectorAll(".play-song").forEach(element => {
+        element.classList.remove("hidden");
+    });
+    // Hide the play animation for all songs
+    document.querySelectorAll(".playin-song").forEach(element => {
+        element.classList.add("hidden");
+    });
+    // Show the play animation for the currently playing song
+    const playBtn = parent.querySelector(".play-song"); 
+    const playAnimation = parent.querySelector(".playin-song"); 
+    playBtn.classList.add("hidden"); // Hide play button
+    playAnimation.classList.remove("hidden"); // Show play animation
+}
